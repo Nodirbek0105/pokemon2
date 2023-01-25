8957a7b04ec694607ebab627aaddcd8e451f4093
-m <-m>
2023-01-25 15:44:30 +0500

    yarimta search with type

--- js/main.js
+++ js/main.js
@@ -5,35 +5,29 @@ var elinput2 = document.querySelector("[data-input2]");
 var elinput3 = document.querySelector("[data-input3]");
 var elinput4 = document.querySelector("[data-input4]");
 var elform = document.querySelector("[data-form]");
-var elTemplate = document.querySelector("template")
+var elTemplate = document.querySelector("template");
 
-// renderPokemon(pokemons)
+renderPokemon(pokemons);
 
-// function renderPokemon(pokemons){
-//   elUL.innerHTML = ""
-//   pokemons.forEach(pokemons => {
-//     elUL.appendChild(createElli(addCard(pokemons)))
-//   });
-// }
+function renderPokemon(pokemons) {
+  elUL.innerHTML = "";
+  for (let i = 0; i < pokemons.length; i++) {
+    const pokemon = pokemons[i];
+    elUL.appendChild(createElli(pokemon));
+  }
+}
 
-// function createElli(){
-//   pokemons.forEach(pokemons => {
-//     var elli = document.createElement("li")
-//     elUL.appendChild(elli)
-//   });
-// }
-
-// function addCard(pokemon){
-//   const elli = elTemplate.content.cloneNode(true)
-//   elli.querySelector("[data-img]").src = pokemon.img
-//   elli.document.querySelector('.name').textContent = pokemon.name
-//   elli.document.querySelector('.type').textContent = pokemon.type
-//   elli.document.querySelector('.weaknesses').textContent = pokemon.weaknesses
-//   elli.document.querySelector('.kg').textContent = pokemon.weight
-//   elli.document.querySelector('.height').textContent = pokemon.height
-//   return elli
-// }
-
+function createElli(pokemon) {
+  const elTemp = elTemplate.content.cloneNode(true);
+  elTemp.querySelector("[data-img]").src = pokemon.img;
+  elTemp.querySelector("[data-img]").alt = `img pokemon ${pokemon.name}`;
+  elTemp.querySelector(".name").textContent = pokemon.name;
+  elTemp.querySelector(".type").textContent = pokemon.type;
+  elTemp.querySelector(".weaknesses").textContent = pokemon.weaknesses;
+  elTemp.querySelector(".kg").textContent = pokemon.weight;
+  elTemp.querySelector(".height").textContent = pokemon.height;
+  return elTemp;
+}
 
 elform.addEventListener("submit", function (evt) {
   evt.preventDefault();
@@ -52,143 +46,10 @@ elform.addEventListener("submit", function (evt) {
   pokemon.height = `${elinput4.value} m`;
 
   pokemons.unshift(pokemon);
-  elUL.innerHTML = "";
-
-  for (i = 0; i < pokemons.length; i++) {
-    const element = pokemons[i].img;
-    const element2 = pokemons[i].name;
-    const element3 = pokemons[i].type;
-    const element4 = pokemons[i].weight;
-    const element5 = pokemons[i].height;
-    var elli = document.createElement("li");
-    var eldivimg = document.createElement("div");
-    var eldiv = document.createElement("div");
-    var eldivright = document.createElement("div");
-    var eldivleft = document.createElement("div");
-    var eldivlefttop = document.createElement("div");
-    var eldivleftbottom = document.createElement("div");
-    var elpname = document.createElement("p");
-    var elptype = document.createElement("p");
-    var elpkg = document.createElement("p");
-    var elpage = document.createElement("p");
-    var elimg = document.createElement("img");
-    var elimgHeart = document.createElement("img");
-
-    elli.classList.add("elli");
-    eldivimg.classList.add("eldivimg");
-    eldiv.classList.add("eldiv");
-    elimgHeart.classList.add("heart");
-    eldivlefttop.classList.add("left-div");
-
-    elimg.setAttribute("src", element);
-    elimgHeart.setAttribute("src", "./img/heart.svg");
-    eldivimg.classList.add("divImg");
-    eldiv.classList.add("divParent");
-    eldivleft.classList.add("divLeft");
-    eldivright.classList.add("divRight");
-    eldivleftbottom.classList.add("divLeftBottom");
-    elpname.classList.add("name");
-    elptype.classList.add("type");
-
-    elpname.textContent = `${element2}`;
-    elptype.textContent = `${element3}`;
-    elpkg.textContent = `${element4}`;
-    elpage.textContent = `${element5}`;
-    elUL.appendChild(elli);
-    elli.appendChild(eldivimg);
-    eldivimg.appendChild(elimg);
-    elli.appendChild(eldiv);
-    eldiv.appendChild(eldivleft);
-    eldiv.appendChild(eldivright);
-    eldivright.appendChild(elimgHeart);
-    eldivleft.appendChild(eldivlefttop);
-    eldivleft.appendChild(eldivleftbottom);
-    eldivlefttop.appendChild(elpname);
-    eldivlefttop.appendChild(elptype);
-    eldivleftbottom.appendChild(elpkg);
-    eldivleftbottom.appendChild(elpage);
-    // elButton.addEventListener("click", function (evt) {
-    //   console.log(pokemon);
-    // });
-  }
+  renderPokemon(pokemons);
   // elinput.value = "";
   elinput1.value = "";
   elinput2.value = "";
   elinput3.value = "";
   elinput4.value = "";
 });
-
-for (i = 0; i < pokemons.length; i++) {
-  const element = pokemons[i].img;
-  const element2 = pokemons[i].name;
-  const element3 = pokemons[i].type;
-  const element4 = pokemons[i].weight;
-  const element5 = pokemons[i].height;
-  const element6 = pokemons[i].weaknesses;
-  var elli = document.createElement("li");
-  var eldivimg = document.createElement("div");
-  var eldiv = document.createElement("div");
-  var eldivright = document.createElement("div");
-  var eldivleft = document.createElement("div");
-  var eldivlefttop = document.createElement("div");
-  var eldivleftbottom = document.createElement("div");
-  var elpname = document.createElement("p");
-  var elptype = document.createElement("p");
-  var elpkg = document.createElement("p");
-  var elpweaknes = document.createElement("p");
-  var elpage = document.createElement("p");
-  var elimg = document.createElement("img");
-  var elimgHeart = document.createElement("img");
-
-  elli.classList.add("elli");
-  eldivimg.classList.add("eldivimg");
-  eldiv.classList.add("eldiv");
-  elimgHeart.classList.add("heart");
-  eldivlefttop.classList.add("left-div");
-
-  elimg.setAttribute("src", element);
-  elimgHeart.setAttribute("src", "./img/heart.svg");
-  eldivimg.classList.add("divImg");
-  eldiv.classList.add("divParent");
-  eldivleft.classList.add("divLeft");
-  eldivright.classList.add("divRight");
-  eldivleftbottom.classList.add("divLeftBottom");
-  elpname.classList.add("name");
-  elptype.classList.add("type");
-
-  elpname.textContent = `${element2}`;
-  elptype.textContent = `${element3}`;
-  elpkg.textContent = `${element4}`;
-  elpage.textContent = `${element5}`;
-  elpweaknes.textContent = element6;
-  elUL.appendChild(elli);
-  elli.appendChild(eldivimg);
-  eldivimg.appendChild(elimg);
-  elli.appendChild(eldiv);
-  eldiv.appendChild(eldivleft);
-  eldiv.appendChild(eldivright);
-  eldivright.appendChild(elimgHeart);
-  eldivleft.appendChild(eldivlefttop);
-  eldivleft.appendChild(eldivleftbottom);
-  eldivlefttop.appendChild(elpname);
-  eldivlefttop.appendChild(elptype);
-  eldivlefttop.appendChild(elpweaknes);
-  eldivleftbottom.appendChild(elpkg);
-  eldivleftbottom.appendChild(elpage);
-  elimgHeart.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    // elButton.addEventListener("click", function (evt) {
-    //   console.log(pokemon);
-    // });
-  });
-}
-
-
-for (let i = 0; i < pokemons.length; i++) {
-  const element = pokemons[i];
-  const element2 = pokemons[i].next_evolution;
-  var arr = []
-  arr.push(element2.length)
-  console.log(arr)
-  
-}
--- js/search.js
+++ js/search.js
@@ -4,150 +4,57 @@ var elinput5 = document.querySelector("[data-input5]");
 var elSelect = document.querySelector("[data-select]");
 var elSearch = document.querySelector("[data-search]");
 var elclose = document.querySelector("[data-close]");
-var all = "all";
 
 elSearch.addEventListener("click", function (evt) {
   evt.preventDefault();
   elform2.style.display = "block";
-  elclose.classList.remove("d-n")
+  elclose.classList.remove("d-n");
   elSearch.setAttribute("class", "d-n");
 });
+
 elclose.addEventListener("click", function (evt) {
   evt.preventDefault();
   elform2.style.display = "none";
-  elclose.classList.add("d-n")
+  elclose.classList.add("d-n");
   elSearch.setAttribute("class", "");
 });
 
 elform2.addEventListener("submit", function (evt) {
   evt.preventDefault();
-  for (i = 0; i < pokemons.length; i++) {
-    const element = pokemons[i];
-    if (elinput5.value.toUpperCase() == element.name.toUpperCase()) {
-      elUL.innerHTML = "";
-      const element = pokemons[i].img;
-      const element2 = pokemons[i].name;
-      const element3 = pokemons[i].type;
-      const element4 = pokemons[i].weight;
-      const element5 = pokemons[i].height;
-      const element6 = pokemons[i].weaknesses;
-      var elli = document.createElement("li");
-      var eldivimg = document.createElement("div");
-      var eldiv = document.createElement("div");
-      var eldivright = document.createElement("div");
-      var eldivleft = document.createElement("div");
-      var eldivlefttop = document.createElement("div");
-      var eldivleftbottom = document.createElement("div");
-      var elpname = document.createElement("p");
-      var elptype = document.createElement("p");
-      var elpkg = document.createElement("p");
-      var elpweaknes = document.createElement("p");
-      var elpage = document.createElement("p");
-      var elimg = document.createElement("img");
-      var elimgHeart = document.createElement("img");
-
-      elli.classList.add("elli");
-      eldivimg.classList.add("eldivimg");
-      eldiv.classList.add("eldiv");
-      elimgHeart.classList.add("heart");
-      eldivlefttop.classList.add("left-div");
-
-      elimg.setAttribute("src", element);
-      elimgHeart.setAttribute("src", "./img/heart.svg");
-      eldivimg.classList.add("divImg");
-      eldiv.classList.add("divParent");
-      eldivleft.classList.add("divLeft");
-      eldivright.classList.add("divRight");
-      eldivleftbottom.classList.add("divLeftBottom");
-      elpname.classList.add("name");
-      elptype.classList.add("type");
-
-      elpname.textContent = `${element2}`;
-      elptype.textContent = `${element3}`;
-      elpkg.textContent = `${element4}`;
-      elpage.textContent = `${element5}`;
-      elpweaknes.textContent = element6;
-      elUL.appendChild(elli);
-      elli.appendChild(eldivimg);
-      eldivimg.appendChild(elimg);
-      elli.appendChild(eldiv);
-      eldiv.appendChild(eldivleft);
-      eldiv.appendChild(eldivright);
-      eldivright.appendChild(elimgHeart);
-      eldivleft.appendChild(eldivlefttop);
-      eldivleft.appendChild(eldivleftbottom);
-      eldivlefttop.appendChild(elpname);
-      eldivlefttop.appendChild(elptype);
-      eldivlefttop.appendChild(elpweaknes);
-      eldivleftbottom.appendChild(elpkg);
-      eldivleftbottom.appendChild(elpage);
-      elinput5.value = "";
-    }
-  }
+  searchPokemon(pokemons);
 });
+
 elform3.addEventListener("submit", function (evt) {
   evt.preventDefault();
-  for (i = 0; i < pokemons.length; i++) {
-    const element = pokemons[i];
-    if (elSelect.value == element.type) {
-      elUL.innerHTML = "";
-      const element = pokemons[i].img;
-      const element2 = pokemons[i].name;
-      const element3 = pokemons[i].type;
-      const element4 = pokemons[i].weight;
-      const element5 = pokemons[i].height;
-      const element6 = pokemons[i].weaknesses;
-      var elli = document.createElement("li");
-      var eldivimg = document.createElement("div");
-      var eldiv = document.createElement("div");
-      var eldivright = document.createElement("div");
-      var eldivleft = document.createElement("div");
-      var eldivlefttop = document.createElement("div");
-      var eldivleftbottom = document.createElement("div");
-      var elpname = document.createElement("p");
-      var elptype = document.createElement("p");
-      var elpkg = document.createElement("p");
-      var elpweaknes = document.createElement("p");
-      var elpage = document.createElement("p");
-      var elimg = document.createElement("img");
-      var elimgHeart = document.createElement("img");
+  typePokemon(pokemons)
+});
 
-      elli.classList.add("elli");
-      eldivimg.classList.add("eldivimg");
-      eldiv.classList.add("eldiv");
-      elimgHeart.classList.add("heart");
-      eldivlefttop.classList.add("left-div");
+function typePokemon(pokemon) {
+  elUL.innerHTML = "";
+  for (let i = 0; i < pokemon.length; i++) {
+    const element = pokemon[i].type;
+    if (elSelect.value == element) {
+      elUL.appendChild(createElli(element))
+    } 
+    if (elSelect.value == element[0]) {
+      elUL.appendChild(createElli(element))
+    } 
+    if (elSelect.value.includes(element[1])) {
+      elUL.appendChild(createElli(element))
+    } 
+    if (elSelect.value.includes(element[2])) {
+      elUL.appendChild(createElli(element))
+    } 
+  }
+}
 
-      elimg.setAttribute("src", element);
-      elimgHeart.setAttribute("src", "./img/heart.svg");
-      eldivimg.classList.add("divImg");
-      eldiv.classList.add("divParent");
-      eldivleft.classList.add("divLeft");
-      eldivright.classList.add("divRight");
-      eldivleftbottom.classList.add("divLeftBottom");
-      elpname.classList.add("name");
-      elptype.classList.add("type");
-
-      elpname.textContent = `${element2}`;
-      elptype.textContent = `${element3}`;
-      elpkg.textContent = `${element4}`;
-      elpage.textContent = `${element5}`;
-      elpweaknes.textContent = element6;
-      elUL.appendChild(elli);
-      elli.appendChild(eldivimg);
-      eldivimg.appendChild(elimg);
-      elli.appendChild(eldiv);
-      eldiv.appendChild(eldivleft);
-      eldiv.appendChild(eldivright);
-      eldivright.appendChild(elimgHeart);
-      eldivleft.appendChild(eldivlefttop);
-      eldivleft.appendChild(eldivleftbottom);
-      eldivlefttop.appendChild(elpname);
-      eldivlefttop.appendChild(elptype);
-      eldivlefttop.appendChild(elpweaknes);
-      eldivleftbottom.appendChild(elpkg);
-      eldivleftbottom.appendChild(elpage);
-      elinput5.value = "";
+function searchPokemon(pokemons) {
+  elUL.innerHTML = "";
+  for (let i = 0; i < pokemons.length; i++) {
+    const pokemon = pokemons[i];
+    // const pokemoni = pokemons;
+    if (elinput5.value.toUpperCase() == pokemon.name.toUpperCase()) {
+      elUL.appendChild(createElli(pokemon));
     }
   }
-});
+}
